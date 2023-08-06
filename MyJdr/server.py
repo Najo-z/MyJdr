b0a5ef828e5d891febee56deadd3bc4dabc53bb0
Jonathan Hugonnard <jonathan.hugonnard@gmail.com>
2023-08-06 18:31:08 +0200

    remove useless

--- MyJdr/server.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-from storage import StorageInterface
-from pathlib import Path
-
-storage = StorageInterface.FileSystemStorage(
-    Path('./tables'), load_tables=True)
-
-storage.print_tables()
