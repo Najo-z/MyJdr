8cf905eaa143fb380c0d15c8e3de1c0b634d70cb
najo-z <jonathan.hugonnard@gmail.com>
2023-08-09 18:24:47 +0000

    b64 encode/decode requests

--- my_jdr/routes/util.py
+++ my_jdr/routes/util.py
@@ -4,6 +4,21 @@ import json
 import random
 import uuid
 from flask import request
+import base64
+
+
+def encryptMessage(message: str) -> str:
+    message_bytes = message.encode('utf-8')
+    base64_bytes = base64.b64encode(message_bytes)
+    base64_message = base64_bytes.decode('utf-8')
+    return base64_message
+
+
+def decryptMessage(message: str) -> str:
+    base64_bytes = message.encode('utf-8')
+    new_message_bytes = base64.b64decode(base64_bytes)
+    new_message = new_message_bytes.decode('utf-8')
+    return new_message
 
 
 def error(uid: str, error: str = "db error", code: int = 400, **kwargs) -> tuple[str, int]:
@@ -13,7 +28,7 @@ def error(uid: str, error: str = "db error", code: int = 400, **kwargs) -> tuple
     }
     for k, v in kwargs.items():
         response[k] = v
-    return json.dumps(response), code
+    return encryptMessage(json.dumps(response)), code
 
 
 def success(uid: str, ok: str = "Success.", code: int = 200, **kwargs) -> tuple[str, int]:
@@ -23,15 +38,16 @@ def success(uid: str, ok: str = "Success.", code: int = 200, **kwargs) -> tuple[
     }
     for k, v in kwargs.items():
         response[k] = v
-    return json.dumps(response), code
+    return encryptMessage(json.dumps(response)), code
 
 
 def check_request(required: tuple[str] = ("uid",)) -> tuple[dict | None, str | None]:
-    if len(request.data) > 100000:
+    decrypted = decryptMessage(request.data.decode('utf-8'))
+    if len(decrypted) > 100000:
         print(
-            f'Large request incoming with size {len(request.data)}. Ignoring.')
+            f'Large request incoming with size {len(decrypted)}. Ignoring.')
         return None, error("-1", "Too large request")
-    data = json.loads(request.data)
+    data = json.loads(decrypted)
     if not all((r in data for r in required)):
         print(
             f'Request with missing one of {required}.')
